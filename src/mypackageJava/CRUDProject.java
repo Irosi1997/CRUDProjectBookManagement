979391b337448a82e795cdd70ee7abc51075c81e
Irosi1997 <84384704+Irosi1997@users.noreply.github.com>
2021-09-27 10:33:21 +0530

    Add files via upload

---
+++ src/mypackageJava/CRUDProject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypackageJava;
+
+import org.nlt.veiw.CRUDVeiwForm;
+
+
+
+
+/**
+ *
+ * @author Thamasha
+ */
+public class CRUDProject {
+    public static void main(String[] args) {
+      new CRUDVeiwForm().setVisible(true);
+    }
+    
+}
